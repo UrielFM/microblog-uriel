0e9165b7c70662ecce179e21702b92f045aa21c1
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-11-16 17:29:29 -0300

    conectando o formualrio com banco de dados

---
+++ inc/funcoes-usuarios.php
@@ -0,0 +1,15 @@
+<?php
+
+require "conecta.php";
+
+function inserirUsuario($conexao, $nome, $email, $senha, $tipo){
+    /* Montando uma variável com o comando SQL de INSERT e com os dados (paràmentros) recebidos pela função */
+    $sql = "INSERT INTO usuarios(nome, email, senha, tipo)
+    VALUES('$nome', '$email', '$senha', '$tipo')";
+
+    /* Executando o comando SQL */
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+}
+
+
+?>
