8d5adce5a8fd1a123c1e26ab86337c1d8f79ed05
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-11-21 14:49:00 -0300

    array

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -11,9 +11,16 @@ function inserirUsuario($conexao, $nome, $email, $senha, $tipo){
     mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 }
 
+// Exibindo os usuarios na pagina
 function lerUsuarios($conexao){
+    /* Comando SQL para fzaer a leitura de dados(SELECT) */
     $sql = "SELECT id, nome, email, tipo FROM usuarios ORDER BY nome";
-}
 
+
+  /* Execução do comando e armazenamento do resultado da consulta/query */
+$resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+// Retornando o resultado da query transformando em array associativo
+return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+}
 
 ?>
