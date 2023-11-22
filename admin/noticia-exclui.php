becb2febf29ef04443acbff083e51344b4c047d8
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-11-22 15:04:38 -0300

    require "funcoes-sessao.php"

---
+++ admin/noticia-exclui.php
@@ -0,0 +1,5 @@
+<?php
+
+require "funcoes-sessao.php";
+verificaAcesso();
+
--- admin/usuario-exclui.php
+++ admin/usuario-exclui.php
@@ -1,6 +1,8 @@
 <?php
-
 require "../inc/funcoes-usuarios.php";
+require "funcoes-sessao.php";
+verificaAcesso();
+
 $id = $_GET['id'];
 excluirUsuario($conexao, $id);
 header("location:usuarios.php");
